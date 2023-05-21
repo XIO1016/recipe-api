467fb48c878f077bf6c3bdcd6c8e207ba5fdb5c6
xio1016 <carpediemsong7@naver.com>
2023-05-21 22:06:52 +0900

    Fix: fix recipe list error

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,15 +5,18 @@ from _decimal import Decimal
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Recipe
 from recipe.serializers import RecipeSerializer
 
+RECIPES_URL = reverse('recipe:recipe-list')
+
 
 def create_recipe(user, **params):
-    """"Create and return a smple recipe."""
+    """Create and return a sample recipe."""
     defaults = {
         'title': 'Sample recipe title',
         'time_minutes': 22,

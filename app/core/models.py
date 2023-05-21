9658487010f2795797d1793a7e9213131e102292
xio1016 <carpediemsong7@naver.com>
2023-05-21 22:02:27 +0900

    Feat: update recipe api

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Databases models.
 """
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -16,7 +17,7 @@ class UserManager(BaseUserManager):
         """Create, save and return a new user."""
         user = self.model(email=self.normalize_email(email), **extra_fields)
         if not email:
-            raise ValueError('User must habe an email address.')
+            raise ValueError('User must have an email address.')
         user.set_password(password)
         user.save(using=self._db)
 
@@ -42,3 +43,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
